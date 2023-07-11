f8430cbd1edff7be86ea1f17dddf0f5a8cbd9c8a
canopy-js-user <47240210+canopy-js-user@users.noreply.github.com>
2023-07-11 12:53:43 -0400

    Line parsing version
    Reverting but keeping for record

--- canopy_parse_data.py
+++ canopy_parse_data.py
@@ -1,16 +1,18 @@
 import pprint
 pp = pprint.PrettyPrinter(indent=4)
 
+def split_text_into_lines_with_indexes(text):
+    lines = text.splitlines()
+    index = 0
+    for line in lines:
+        line_with_index = (index, line)
+        index += len(line)
+        yield line_with_index
+
 def parse_file(self, sublime):
   print('parsing')
   canopy_parse_data = {}
   text = self.view.substr(sublime.Region(0, self.view.size()))
-
-  state = 'initial'
-  current_category = None
-  current_topic = None
-  current_subtopic = None
-  current_reference = None
 
   categories = []
   topics = []
@@ -29,261 +31,31 @@ def parse_file(self, sublime):
   subtopics_by_name = {}
   references_by_target = {}
 
-  buffer = ''
-  i = 0
+  lines = split_text_into_lines_with_indexes(text)
 
-  while i < len(text):
-    # print(i, state, text[i].encode('utf-8'))
-    # print(subtopics_by_index)
-    if state == 'initial':
-      if text[i] == '[':
-        state = 'in_category_name'
-        current_category = {}
-        current_category['start'] = i
-        categories_by_index[i] = current_category
-      else:
-        return
+  current_category = None
+  current_topic = None
+  current_subtopic = None
 
-    elif state == 'in_category_name':
-      if text[i] == ']' and i < len(text) - 1 and text[i + 1] == '\n':
-        current_category['name'] = buffer
-        categories.append(current_category)
-        categories_by_name[self.remove_markdown(buffer)] = current_category
-        categories_by_index[i] = current_category
-        categories_by_index[i + 1] = current_category
-        buffer = ''
-        i += 1
-        state = 'in_category'
-      else:
-        categories_by_index[i] = current_category
-        buffer += text[i]
+  for index, line in lines:
+    category_match = re.match(r"^\[(\w+(?:\/\w+)+)\]$", line)
+    topic_match = re.match(r"^\* [^\n]+(\?|(?=:))$", line)
+    subtopic_match = re.match(r"^[^\n]+(\?|(?=:))$", line)
 
-    elif state == 'in_category':
-      if text[i - 1] == '\n' and text[i] == '*' and text[i + 1] == ' ':
-        current_topic = {}
-        current_subtopic = {}
-        current_topic['start'] = i
-        current_subtopic['start'] = i
+    if category_match:
+      current_category = { 'name': category_match[1], 'start': index }
 
-        topic_definitions_by_index[i] = current_topic
-        topic_definitions_by_index[i + 1] = current_topic
-        subtopic_definitions_by_index[i] = current_subtopic
-        subtopic_definitions_by_index[i + 1] = current_subtopic
+    elif topic_match:
+      current_topic = { 'name': topic_match[1], 'start': index }
 
-        topics_by_index[i] = current_topic
-        topics_by_index[i + 1] = current_topic
-        subtopics_by_index[i] = current_subtopic
-        subtopics_by_index[i + 1] = current_subtopic
-        categories_by_index[i] = current_category
-        categories_by_index[i + 1] = current_category
+    elif subtopic_match:
+      current_subtopic = { 'name': subtopic_match[1], 'start': index }
 
-        i += 1
-        state = 'in_topic_name'
+    else:
+      categories_by_index
+      topics_by_index
+      subtopics_by_index
 
-      else:
-        categories_by_index[i] = current_category
-
-    elif state == 'in_topic_name':
-      if (text[i] == ':' or text[i] == '?') and text[i - 1] != '\\' and (i < len(text) - 1 and text[i + 1] == ' ' or text[i + 1] == '\n'):
-        current_topic['name'] = buffer
-        current_subtopic['name'] = buffer
-        current_subtopic['topic'] = current_topic
-        current_topic['category'] = current_category
-        current_subtopic['category'] = current_category
-
-        topics_by_index[i] = current_topic
-        topics_by_index[i + 1] = current_topic
-
-        subtopics_by_index[i] = current_subtopic
-        subtopics_by_index[i + 1] = current_subtopic
-
-        categories_by_index[i] = current_category
-        categories_by_index[i + 1] = current_category
-
-        topic_definitions_by_index[i] = current_topic
-        topic_definitions_by_index[i + 1] = current_topic
-
-        subtopic_definitions_by_index[i] = current_subtopic
-        subtopic_definitions_by_index[i + 1] = current_subtopic
-
-        topics.append(current_topic)
-        topics_by_name[self.remove_markdown(buffer)] = current_topic
-        subtopics.append(current_subtopic)
-        subtopics_by_name[self.remove_markdown(buffer)] = current_subtopic
-
-        buffer = ''
-
-        if text[i + 1] == ' ' and text[i + 2] != '\n':
-          state = 'in_paragraph'
-        else:
-          state = 'in_topic'
-
-        i += 1
-
-      elif text[i] == '\n':
-        categories_by_index[i] = current_category
-
-        buffer = ''
-        state = 'in_note'
-
-      else:
-        topic_definitions_by_index[i] = current_topic
-        subtopic_definitions_by_index[i] = current_subtopic
-        topics_by_index[i] = current_topic
-        subtopics_by_index[i] = current_subtopic
-        categories_by_index[i] = current_category
-
-        buffer += text[i]
-
-    elif state == 'in_paragraph':
-      if text[i] == '\n' and i < len(text) - 1 and text[i + 1] == '\n':
-        topics_by_index[i] = current_topic
-        topics_by_index[i + 1] = current_topic
-        subtopics_by_index[i] = current_subtopic
-        subtopics_by_index[i + 1] = current_subtopic
-        categories_by_index[i] = current_category
-        categories_by_index[i + 1] = current_category
-        current_subtopic['end'] = i
-
-        state = 'in_topic'
-
-      elif text[i] == '[' and i < len(text) - 1 and text[i + 1] == '[':
-        current_reference = {}
-        current_reference['start'] = i
-        references_by_index[i] = current_reference
-        topics_by_index[i] = current_topic
-        subtopics_by_index[i] = current_subtopic
-        categories_by_index[i] = current_category
-        i += 1
-        state = 'in_reference'
-
-      else:
-        topics_by_index[i] = current_topic
-        subtopics_by_index[i] = current_subtopic
-        categories_by_index[i] = current_category
-
-    elif state == 'in_topic':
-      if text[i] == '*' and i < len(text) - 1 and text[i + 1] == ' ':
-        current_topic['end'] = i - 1
-        current_topic = {}
-        current_topic['start'] = i
-        current_subtopic = {}
-        current_subtopic['start'] = i
-
-        topic_definitions_by_index[i] = current_topic
-        topic_definitions_by_index[i + 1] = current_topic
-        subtopic_definitions_by_index[i] = current_subtopic
-        subtopic_definitions_by_index[i + 1] = current_subtopic
-
-        topics_by_index[i] = current_topic
-        topics_by_index[i + 1] = current_topic
-        subtopics_by_index[i] = current_subtopic
-        subtopics_by_index[i + 1] = current_subtopic
-        categories_by_index[i] = current_category
-        categories_by_index[i + 1] = current_category
-
-        i += 1
-        state = 'in_topic_name'
-
-      elif text[i] == '[':
-        current_category['end'] = i - 1
-        current_category = {}
-        current_category['start'] = i
-        current_topic['end'] = i - 1
-        current_topic = None
-
-        categories_by_index[i] = current_category
-
-        state = 'in_category_name'
-
-      elif text[i] != '*' and text[i] != '\n' and text[i] != '[':
-        buffer += text[i]
-        current_subtopic = {}
-        current_subtopic['start'] = i
-        subtopic_definitions_by_index[i] = current_subtopic
-
-        topics_by_index[i] = current_topic
-        subtopics_by_index[i] = current_subtopic
-        categories_by_index[i] = current_category
-
-        state = 'in_subtopic_name'
-      else:
-        topics_by_index[i] = current_topic
-        categories_by_index[i] = current_category
-
-    elif state == 'in_subtopic_name':
-      if (text[i] == ':' or text[i] == '?') and text[i - 1] != '\\' and (i < len(text) - 1 and text[i + 1] == ' ' or text[i + 1] == '\n'):
-        current_subtopic['name'] = buffer
-        current_subtopic['topic'] = current_topic
-        current_subtopic['category'] = current_category
-
-        subtopic_definitions_by_index[i] = current_subtopic
-        subtopics.append(current_subtopic)
-        subtopics_by_name[self.remove_markdown(buffer)] = current_subtopic
-
-        topics_by_index[i] = current_topic
-        subtopics_by_index[i] = current_subtopic
-        categories_by_index[i] = current_category
-
-        buffer = ''
-        state = 'in_paragraph'
-
-      elif text[i] == '\n' or (text[i] == '.' and text[i - 1] != '\\') or (text[i] == ',' and text[i - 1] != '\\'):
-        subtopic_definitions_by_index[i] = current_subtopic
-        topics_by_index[i] = current_topic
-        categories_by_index[i] = current_category
-
-        buffer = ''
-        state = 'in_note'
-
-      else:
-        buffer += text[i]
-
-    elif state == 'in_note':
-      if text[i] == '\n' and ((i < len(text) - 1 and text[i + 1] == '\n') or (text[i - 1] == '\n')):
-        state = 'in_topic'
-        topics_by_index[i] = current_topic
-        categories_by_index[i] = current_category
-
-    elif state == 'in_reference':
-      if text[i] == ']' and i < len(text) - 1 and text [i + 1] == ']':
-        current_reference['text'] = buffer
-        current_reference['target'] = self.render(buffer)
-        current_reference['end'] = i + 2
-        current_reference['subtopic'] = current_subtopic
-        current_reference['topic'] = current_topic
-        current_reference['category'] = current_category
-
-        references_by_index[i] = current_reference
-        references_by_index[i + 1] = current_reference
-        references_by_index[i + 2] = current_reference
-
-        topics_by_index[i] = current_topic
-        topics_by_index[i + 1] = current_topic
-        subtopics_by_index[i] = current_subtopic
-        subtopics_by_index[i + 1] = current_subtopic
-        categories_by_index[i] = current_category
-        categories_by_index[i + 1] = current_category
-
-        references.append(current_reference)
-        current_reference = {}
-
-        references_by_target[self.render(buffer)] = current_reference
-
-        buffer = ''
-        i += 1
-
-        state = 'in_paragraph'
-      else:
-        references_by_index[i] = current_reference
-
-        topics_by_index[i] = current_topic
-        subtopics_by_index[i] = current_subtopic
-        categories_by_index[i] = current_category
-
-        buffer += text[i]
-
-    i += 1
 
   current_category['end'] = i
   categories_by_index[i] = current_category
